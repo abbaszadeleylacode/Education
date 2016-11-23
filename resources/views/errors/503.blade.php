d537424eebe6562be39ae6a6f0ba31b65e2ad3a6
Murad Mikayilzadeh <Murad Mikayilzadeh>
2016-11-23 23:20:16 +0400

    23-11-2016 23-20

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Be right back.</title>
+<head>
+    <title>Səhifə tapılmadı</title>
+    <title>Siniflər(Dərs Cədvəlləri)</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{url('assets/vendor/bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('assets/vendor/font-awesome/css/font-awesome.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/admin.css')}}">
+    <script src="{{url('assets/vendor/jquery-3.1.1.min.js')}}"></script>
+    <style>
+        .sagirdButton{
+            margin-top: 30px;
+        }
+        img{
+            height: 40px;
+        }
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
+        h2{
                 width: 100%;
-                color: #B0BEC5;
+                color: #0AAAE7;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
+        }
+    </style>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="logo col-md-12">
+            <img src="{{url('assets/images/logo.png')}}" alt="">
         </div>
-    </body>
+    </div>
+
+    <h2>404 səhifə tapılmadı!</h2>
+    <p>Axtarışınıza dair heç bir nəticə tapılmadı. Zəhmət,olmasa axtardığınız başlığın doğru olub-olmadığını yoxlayın.</p>
+</div>
+</body>
 </html>

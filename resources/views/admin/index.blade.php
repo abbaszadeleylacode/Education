b391d9773717bfaf4cf40a58d315a69e92b8b959
Leyla Abbasova <Leyla Abbasova>
2017-01-23 14:37:27 +0400

    23.01.17  14:37

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,13 +9,14 @@
 	<link rel="stylesheet" href="{{url('assets/css/admin.css')}}">
 	<script src="{{url('assets/vendor/jquery-3.1.1.min.js')}}"></script>
 </head>
+
 <body>
 
 <div class="container-fluid">
 	
 	<div class="row">
-		<div class="logo col-md-12">
-			<img src="{{url('assets/images/logo.png')}}">	
+		<div class="logo col-md-12 col-sm-12 col-xs-12">
+			<img src="{{url('assets/images/logo.png')}}" class="img">	
 			<a class="btn btn-info" href="{{url('')}}">Çıxış et</a>
 		</div>
 		
@@ -38,76 +39,93 @@
 			});
 		</script>
 
-		<div class="sagird col-md-3 donenHisseler col-sm-12 col-xs-12">
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class  sagird donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('sagirdsiyahisi')}}">
-					<img src="{{url('assets/images/admin/students.jpg')}}" title="Şagirdlər">
+					<img src="{{url('assets/images/admin/students.jpg')}}" title="Şagirdlər" class="img img-responsive"  >
 				</a>
 			</div>
 		</div>
 
-		<div class="quiz col-md-2 donenHisseler col-sm-12 col-xs-12">
+<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class  quiz donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('quiz-admin')}}">
-					<img src="{{url('assets/images/admin/quizzes.jpg')}}" title="Quizlər">
+					<img src="{{url('assets/images/admin/quizzes.jpg')}}" title="Quizlər" class="img img-responsive"  >
 				</a>
 			</div>
 		</div>
 
-		<div class="meeting col-md-2 donenHisseler col-sm-12 col-xs-12">
-			<div class="row">
-				<a href="{{url('meeting-admin')}}">
-					<img src="{{url('assets/images/admin/meeting.jpg')}}" title="Yığıncaqlar">
-				</a>
-			</div>
-		</div>
-
-		<div class="dersCedveli col-md-2 donenHisseler col-sm-12 col-xs-12">
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class  dersCedveli donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('derscedveli')}}">
-					<img src="{{url('assets/images/admin/schedule.jpg')}}" title="Dərs Cədvəli">
+					<img src="{{url('assets/images/admin/schedule.jpg')}}" title="Dərs Cədvəli" class="img img-responsive"  >
+				</a>
+			</div>
+		</div>
+<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class  meeting donenHisseler  var idi burda--}}
+			<div class="row">
+				<a href="{{url('meeting-admin')}}">
+					<img src="{{url('assets/images/admin/meeting.jpg')}}" title="Yığıncaqlar" class="img img-responsive"  >
 				</a>
 			</div>
 		</div>
 
-		<div class="muellimler col-md-3 donenHisseler col-sm-12 col-xs-12">
+
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class muellimler  donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('muellimsiyahisi')}}">
-					<img src="{{url('assets/images/admin/teachers.jpg')}}" title="Müəllimlər">
+					<img src="{{url('assets/images/admin/teachers.jpg')}}" title="Müəllimlər" class="img img-responsive"  >
 				</a>
 			</div>
 		</div>
 
-		<div class="valideyn col-md-3 donenHisseler col-sm-12 col-xs-12">
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{--  class  valideyn donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('admin-parents')}}">
-					<img src="{{url('assets/images/admin/parents.jpg')}}" title="Valideynlər">
+					<img src="{{url('assets/images/admin/parents.jpg')}}" title="Valideynlər"  class="img img-responsive" >
 				</a>
 			</div>
 		</div>
+	
 
-		<div class="elaqe col-md-6 donenHisseler col-sm-12 col-xs-12">
-			<div class="row">
-				<a href="{{url('elaqe-admin')}}">
-					<img src="{{url('assets/images/admin/contact.jpg')}}" title="Əlaqə">
-				</a>
-			</div>
-		</div>
-
-		<div class="imtahan col-md-3 donenHisseler col-sm-12 col-xs-12">
+	
+		
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class imtahan  donenHisseler  var idi burda--}}
 			<div class="row">
 				<a href="{{url('imtahanlar-admin')}}">
-					<img src="{{url('assets/images/admin/exams.jpg')}}" title="İmtahanlar">
+					<img src="{{url('assets/images/admin/exams.jpg')}}" title="İmtahanlar"  class="img img-responsive" >
 				</a>
 			</div>
 		</div>
+
+		<div class=" col-md-3  col-sm-12 col-xs-12">    {{-- class  elaqe  donenHisseler  var idi burda--}}
+			<div class="row">
+				<a href="{{url('elaqe-admin')}}">
+					<img src="{{url('assets/images/admin/contact.jpg')}}" title="Əlaqə"  class="img img-responsive" >
+				</a>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+		
+
+
+		
 
 
 
 
 
 		{{-- Mobile ucun olan hisseler --}}
-		<ul class="col-md-10 col-md-offset-1">
+		<!-- <ul class="col-md-10 col-md-offset-1">
 			<li class="col-md-10 col-md-offset-1">
 				<a href="{{url('sagirdsiyahisi')}}">
 					<img src="{{url('assets/images/admin/students.jpg')}}" title="Şagirdlər" class="img img-responsive">
@@ -155,7 +173,7 @@
 					<img src="{{url('assets/images/admin/contact.jpg')}}" title="Əlaqə" class="img img-responsive">
 				</a>
 			</li>
-		</ul>
+		</ul> -->
 	</div>
 
 </div>
